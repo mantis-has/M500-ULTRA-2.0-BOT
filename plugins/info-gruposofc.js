030b66895bdbaf98ea734f545425b98ad83ef8aa
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-06-27 21:29:57 -0500

    Hecho por SoyMaycol

---
+++ plugins/info-gruposofc.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn }) => {
+
+  const namegrupo = 'Grupo Oficial'
+  const gp1 = 'https://chat.whatsapp.com/KoJjHo6o3Ew7P5qkjaIh0r' // ← tu link real
+
+  const namechannel = 'Canal de la Bot'
+  const channel = 'https://whatsapp.com/channel/0029VbAZcyIIXnlwp79iwu2l' // ← tu canal real
+
+  const dev = '💎 Creador: Félix Manuel'
+  const catalogo = 'https://qu.ax/dXOUo.jpg' // o './media/grupos.jpg'
+  const emojis = '👨‍💻'
+
+  let grupos = `
+╭─⟪ *💎GRUPOS OFICIALES * 
+│
+│ 🩵 *${namegrupo}*
+│ ${gp1}
+│
+│ 🩵 *${namechannel}*
+│ ${channel}
+╰─────────────────╯
+`
+
+  await conn.sendFile(m.chat, catalogo, 'grupos.jpg', grupos.trim(), m)
+  await m.react(emojis)
+}
+
+handler.help = ['grupos']
+handler.tags = ['info']
+handler.command = ['grupos', 'links', 'groups']
+
+export default handler
