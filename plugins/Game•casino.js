e2311a02d319b62ae02ce9ccce9bb2e34b07760b
mantis-has <mantisbotmd@gmail.com>
2025-07-05 21:20:45 -0400

    Actualizar Game•casino.js

--- plugins/Game•casino.js
+++ plugins/Game•casino.js
@@ -15,24 +15,24 @@ let username = conn.getName(who)
 let tiempoEspera = 15
 if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
 let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
-conn.reply(m.chat, `🍟 Ya has iniciado una apuesta recientemente, espera *⏱️ ${tiempoRestante}* para apostar nuevamente`, m, rcanal)
+conn.reply(m.chat, `🩵 Ya has iniciado una apuesta recientemente, espera *⏱️ ${tiempoRestante}* para apostar nuevamente`, m, rcanal)
 return
 }
 cooldowns[m.sender] = Date.now()
 count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
 count = Math.max(1, count)
-if (args.length < 1) return conn.reply(m.chat, '🚩 Ingresa la cantidad de ' + `🍪 *Cookies*` + ' que deseas aportar contra' + ` *blackclover-MD*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
+if (args.length < 1) return conn.reply(m.chat, '🩵 Ingresa la cantidad de ' + `💎 *Diamantes*` + ' que deseas aportar contra' + ` *Makima-MD*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
 if (user.cookies >= count * 1) {
 user.cookies -= count * 1
 if (Aku > Kamu) {
-conn.reply(m.chat, '`🍟 Veamos que numeros tienen!`\n\n'+ `➠ *blackclover* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 🍪 Cookies.`.trim(), m, rcanal)
+conn.reply(m.chat, '`🩵 Veamos que numeros tienen!`\n\n'+ `➠ *Makima* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *PERDISTE* ${formatNumber(count)} 💎 Diamantes.`.trim(), m, rcanal)
 } else if (Aku < Kamu) {
 user.cookies += count * 2
-conn.reply(m.chat, '`🍟 Veamos que numeros tienen!`\n\n'+ `➠ *blackcloverBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 🍪 Cookies.`.trim(), m, rcanal)
+conn.reply(m.chat, '`🩵 Veamos que numeros tienen!`\n\n'+ `➠ *blackcloverBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username}, *GANASTE* ${formatNumber(count * 2)} 💎 Diamantes.`.trim(), m, rcanal)
 } else {
 user.cookies += count * 1
-conn.reply(m.chat, '`🍟 Veamos que numeros tienen!`\n\n'+ `➠ *blackcloverBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 🍪 Cookies.`.trim(), m, rcanal)}
-} else conn.reply(m.chat, `No tienes *${formatNumber(count)} 🍪 Cookies* para apostar!`.trim(), m, rcanal)}
+conn.reply(m.chat, '`🩵 Veamos que numeros tienen!`\n\n'+ `➠ *MakimaBot* : ${Aku}\n➠ *${username}* : ${Kamu}\n\n> ${username} obtienes ${formatNumber(count * 1)} 💎 Diamantes.`.trim(), m, rcanal)}
+} else conn.reply(m.chat, `No tienes *${formatNumber(count)} 💎 Diamantes* para apostar!`.trim(), m, rcanal)}
 
 handler.help = ['apostar *<cantidad>*']
 handler.tags = ['fun']
