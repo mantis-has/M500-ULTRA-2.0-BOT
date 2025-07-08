2ecda577f4e76b9dbff91c0e6296575c10fac286
mantis-has <mantisbotmd@gmail.com>
2025-07-08 09:36:01 -0400

    Actualizar config-nable.js

--- plugins/config-nable.js
+++ plugins/config-nable.js
@@ -2,7 +2,7 @@ const handler = async (m, {conn, usedPrefix, command, args, isOwner, isAdmin, is
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
-const miniopcion = `CONFIGURATION OF GROUPS
+const miniopcion = `CONFIGURACIÓN PARA GRUPOS
 
 ${usedPrefix + command} welcome
 ${usedPrefix + command} autoresponder
@@ -22,7 +22,7 @@ ${usedPrefix + command} modoadmin
 ${usedPrefix + command} antifake
 ${usedPrefix + command} antibot
 
-OPTIONS FOR MY CREATOR
+💎 CONFIGURACIÓN PARA MI CREADOR 
 
 ${usedPrefix + command} antisubots
 ${usedPrefix + command} public
