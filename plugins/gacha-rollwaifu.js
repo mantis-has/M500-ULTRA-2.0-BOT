5db1f1498b1f932b152bf2102d5a9f65be1c96a0
mantis-has <mantisbotmd@gmail.com>
2025-07-08 17:55:17 -0400

    Actualizar gacha-rollwaifu.js

--- plugins/gacha-rollwaifu.js
+++ plugins/gacha-rollwaifu.js
@@ -10,7 +10,7 @@ async function loadCharacters() {
         const data = await fs.readFile(charactersFilePath, 'utf-8')
         return JSON.parse(data)
     } catch (error) {
-        throw new Error('❀ No se pudo cargar el archivo characters.json.')
+        throw new Error('💎 No se pudo cargar el archivo characters.json.')
     }
 }
 
@@ -18,7 +18,7 @@ async function saveCharacters(characters) {
     try {
         await fs.writeFile(charactersFilePath, JSON.stringify(characters, null, 2), 'utf-8')
     } catch (error) {
-        throw new Error('❀ No se pudo guardar el archivo characters.json.')
+        throw new Error('💎 No se pudo guardar el archivo characters.json.')
     }
 }
 
@@ -35,7 +35,7 @@ async function saveHarem(harem) {
     try {
         await fs.writeFile(haremFilePath, JSON.stringify(harem, null, 2), 'utf-8')
     } catch (error) {
-        throw new Error('❀ No se pudo guardar el archivo harem.json.')
+        throw new Error('💎 No se pudo guardar el archivo harem.json.')
     }
 }
 
@@ -61,12 +61,14 @@ let handler = async (m, { conn }) => {
             ? `Reclamado por @${randomCharacter.user.split('@')[0]}` 
             : 'Libre'
 
-        const message = `❀ Nombre » *${randomCharacter.name}*
-⚥ Género » *${randomCharacter.gender}*
-✰ Valor » *${randomCharacter.value}*
-♡ Estado » ${statusMessage}
-❖ Fuente » *${randomCharacter.source}*
-✦ ID: *${randomCharacter.id}*`
+        const message = `╭━━━━━━━━━━━━━━╮
+┃💎 Nombre » *${randomCharacter.name}*
+┃💎 Género: *${randomCharacter.gender}*
+┃💎 Valor: *${randomCharacter.value}*
+┃💎 Estado: ${statusMessage}
+┃💎 Fuente: *${randomCharacter.source}*
+┃💎 ID: *${randomCharacter.id}*
+╰━━━━━━━━━━━━━━━`
 
         const mentions = userEntry ? [userEntry.userId] : []
         await conn.sendFile(m.chat, randomImage, `${randomCharacter.name}.jpg`, message, m, { mentions })
