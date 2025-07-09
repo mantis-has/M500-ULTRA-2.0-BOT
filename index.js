364fd07129abb2de95e3afbbe7b95d9af75a0002
mantis-has <mantisbotmd@gmail.com>
2025-07-09 01:02:15 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ if (opcion == '1' || methodCodeQR) {
 console.log(chalk.bold.yellow(`\n❐ ESCANEA EL CÓDIGO QR EXPIRA EN 45 SEGUNDOS`))}
 }
 if (connection == 'open') {
-console.log(chalk.bold.green('\n✞ black clover Conectado con éxito ✞'))
+console.log(chalk.bold.green('\nConexión establecida con Makima'))
 }
 let reason = new Boom(lastDisconnect?.error)?.output?.statusCode
 if (connection === 'close') {
