c59441c682be323838fcd5c65128f83151b248bc
mantis-has <mantisbotmd@gmail.com>
2025-07-09 00:43:52 -0400

    Actualizar Info•bot.js

--- plugins/Info•bot.js
+++ plugins/Info•bot.js
@@ -5,12 +5,12 @@ handler.all = async function(m) {
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `🩵 ¡Hola! Soy makima, en que puedo ayudarte hoy?\n\n✰ Usa *#menu* para ver mis comandos.`, m, rcanal, )
+conn.reply(m.chat, `🩵 ¡Hola! en que puedo ayudarte hoy?\n\n✰ Usa *#menu* para ver mis comandos.`, m, rcanal, )
 }
 /*if (/^corin|corín|corinplus|corínplus|corinplushost|corínplushost|plus$/i.test(m.text)) {
 conn.reply(m.chat, `🚀 CorinPlus Hosting ¡El plus que necesitas!\n2 *Dash:* https://dash.skyultraplus.com\n🌱 *Panel:* https://ctrl.skyultraplus.com`, m, rcanal, )
 }*/
-if (/^makima$/i.test(m.text)) {
+if (/^oye$/i.test(m.text)) {
 conn.reply(m.chat, `¿Que quieres hijo de puta?`, m, rcanal, )
 }
 if (/^tetas|teta$/i.test(m.text)) {
